e3bbbd0c70d17318d2ffe7b708a36dfb46a3d26c
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-12-09 16:03:22 +0200

    Add project files.

---
+++ Cursor keys/Source.cpp
@@ -0,0 +1,41 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD cursor;
+	cursor.X = 0;
+	cursor.Y = 0;
+	cout << char(2);
+	while (true)
+	{
+		int result = _getch();
+		if (result == 224)
+		{
+			system("cls");
+			result = _getch();
+			switch (result)
+			{
+			case 72: // код клавиши UP
+				cursor.Y--;
+				break;
+			case 75: // код клавиши LEFT
+				cursor.X--;
+				break;
+			case 77: // код клавиши RIGHT
+				cursor.X++;
+				break;
+			case 80: // код клавиши DOWN
+				cursor.Y++;
+				break;
+			}
+			SetConsoleCursorPosition(h, cursor);
+			cout << char(2);
+		}
+	}
+
+	return 0;
+}
